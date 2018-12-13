e2263f277669f4a8bf3f80cba7f673ff82610394
Carlo THeunissen <carlotheunissen96@gmail.com>
2018-12-13 23:15:46 +0100

    clean master

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-docker stop  application
-docker rm -f application
-docker image prune -f
-docker build -t application .
-docker run -d -p 8090:80 --name application application
-docker stop  application
-docker start -a application
